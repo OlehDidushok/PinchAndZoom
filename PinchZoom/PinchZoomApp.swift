ec9a46206313b5a8959b4c221e36928324363ab9
“Oleh <o.didushok@lampalampa.net>
2024-06-04 19:00:13 +0300

    Configured code

---
+++ PinchZoom/PinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomApp.swift
+//  PinchZoom
+//
+//  Created by Oleh on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
